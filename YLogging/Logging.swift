76032e60be9147a2ef37aa63fa80ab3afde4a0f3
Hashfi Alfian Ciyuda <hashfi.51.104@gmail.com>
2018-04-03 14:03:16 +0700

    Initial commit

---
+++ YLogging/Logging.swift
@@ -0,0 +1,33 @@
+//
+//  Logging.swift
+//  YLogging
+//
+//  Created by Hashfi Alfian Ciyuda on 03/04/18.
+//  Copyright © 2018 Hashfi Alfian Ciyuda. All rights reserved.
+//
+
+import Foundation
+
+public class YLog {
+    private var isDebug: Bool!
+    
+    //2.
+    public init() {
+        self.isDebug = false
+    }
+    
+    //3.
+    public func setup(isDebug: Bool) {
+        self.isDebug = isDebug
+        print("Project is in Debug mode: \(isDebug)")
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+    }
+}
---
+++ YLogging/YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by Hashfi Alfian Ciyuda on 03/04/18.
+//  Copyright © 2018 Hashfi Alfian Ciyuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+
